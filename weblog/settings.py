023e54ad60153a33db9bfbb222228dd55c8a1daf
ghadir <ghadir.n.2001@gmail.com>
2025-06-08 02:25:53 +0430

    pythonanywhere

--- weblog/settings.py
+++ weblog/settings.py
@@ -141,5 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+STATIC_ROOT=os.path.join(BASE_DIR , "assets")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
